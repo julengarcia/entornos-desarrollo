530ce56e69566303d254c3eb57e97ec2763b13cf
julen <julen@172.30.0.65>
2015-03-17 12:12:03 +0100

    Second commit

--- GitHub/src/org/cuatrovientos/github/Main.java
+++ GitHub/src/org/cuatrovientos/github/Main.java
@@ -3,7 +3,7 @@ package org.cuatrovientos.github;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Hola y adios");
 
 	}
 
---
+++ GitHub/src/org/cuatrovientos/github/Other.java
@@ -0,0 +1,5 @@
+package org.cuatrovientos.github;
+
+public class Other {
+	
+}
